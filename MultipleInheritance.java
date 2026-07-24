0b9f8b3f9dfb3508e6c42b26310208c7c97b8f88
pranaliBmore <morepranali0007@gmail.com>
2026-07-24 10:54:08 +0530

    Create Vehicle interface

---
+++ MultipleInheritance.java
@@ -0,0 +1,13 @@
+/*Create Vehicle Interface with name, maxPassanger, and maxSpeed variables. Create
+LandVehicle and SeaVehicleinteface from Vehicle interface. LandVehicle has numWheels
+variable and drive method. SeaVehicle has displacement variable and launch method. Create
+Car class from LandVehicle, HoverCraft from LandVehicle and SeaVehicle interface. Also create
+Ship from SeaVehicle. Provide additional methods in HoverCraft as enterLand and enterSea.
+Similarly provide other methods for class Car and Ship. Demonstrate all classes in a application.*/
+
+interface Vehicle {
+    String name = "Default Vehicle";
+    int maxPassanger = 4;
+    int maxSpeed = 120;
+}
+
