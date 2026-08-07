1e80c5c55de343e9f9d5ca76e281127b097e1195
pranaliBmore <morepranali0007@gmail.com>
2026-08-07 09:35:13 +0530

    Stack Interface

---
+++ Stack_Interface.java
@@ -0,0 +1,12 @@
+/*Create the interface stack which has variable size, abstract methods push (), pop (), display (), 
+overflow () and underflow (). We need to implement 3 subclasses IntegerStack, StringStack and 
+Double Stack respectively by implementing interface. All the methods in interface are declared 
+for string. And in subclass for integer Stack convert string to integer. Same thing to all other. 
+Create one test class and check for the working of all the classes. */
+
+
+interface Stack
+{
+    
+}
+
