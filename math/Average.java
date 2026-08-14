cc2245bf299c95cf031c287e081cf924b1475ef3
pranaliBmore <morepranali0007@gmail.com>
2026-08-14 10:33:15 +0530

    Add Mean and Average classes

---
+++ math/Average.java
@@ -0,0 +1,10 @@
+package math;
+import math.Mean;
+
+public class Average {
+    public double calculate(int arr[])
+    {
+        Mean mean = new Mean();
+        return mean.calculate(arr);
+    }
+}
