1d0ad982fa6e59c6231d66b80a7c582fb2f76e52
pranaliBmore <morepranali0007@gmail.com>
2026-08-14 10:33:52 +0530

    Add Median calculation

---
+++ math/Median.java
@@ -0,0 +1,17 @@
+package math;
+
+public class Median {
+    public double calculate(int arr[])
+    {
+        Arrays.sort(arr);
+        int n = arr.length;
+        if (n % 2 == 0)
+        {
+            return (arr[n / 2 - 1 ] + arr[n / 2]) / 2.0;
+        }
+        else
+        {
+            return arr[n / 2];
+        }
+    }
+}
