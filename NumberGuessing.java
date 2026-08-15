e7843e060d7a1719ff781ac0c8cfcb61892942aa
pranaliBmore <morepranali0007@gmail.com>
2026-08-15 21:08:59 +0530

    Add Java number guessing system

---
+++ NumberGuessing.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class NumberGuessing {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Random ra = new Random();
+
+        //Generate a random number between 1 ansd 100
+        int number = ra.nextInt(100) + 1;
+        int guess;
+        int attempts = 0;
+
+        System.out.println("========== Number Guessing Game ==========");
+        System.out.println("Guess a number between 1 and 100: ");
+
+        do {
+            System.out.print("Enter your guess: ");
+            guess = sc.nextInt();
+            attempts++;
+
+            if (guess > number){
+                System.out.println("Too High! Try again.");
+            }     
+           else if (guess < number) {
+            System.out.println("Too Low! Try again.");
+           }
+           else {
+            System.out.println("Congratulations!!! You guessed it!");
+            System.out.println("The number was: " + number);
+            System.out.println("Number of attempts: " + attempts);
+           }
+       }while (guess != number);
+    }
+}
